2b09ed750ef164abf6266e1bc052db31cb4d6ed2
David Valachovic <AdenFlorian@gmail.com>
2016-11-14 20:47:52 -0600

    first commit, with unity project

---
+++ Assets/Squeeze/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) {
+			Move(0, 1);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			Move(0, -1);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			Move(-1, 0);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			Move(1, 0);
+		}
+	}
+
+	void Move(float xAmount, float zAmount)
+	{
+		transform.Translate(xAmount * Time.deltaTime, 0, zAmount * Time.deltaTime);
+	}
+}
